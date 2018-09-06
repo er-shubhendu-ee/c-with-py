5747f941b256d85fd649c887ba5aadb47f4c6063
Shubhendu <er.shubhendu.ee@gmail.com>
2018-09-07 01:04:27 +0530

    functioning
    Functioning code accepting only one line of python code at a time. Working towards working with a .py file.

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print(x)
+'''py_function.py - Python source designed to '''
+'''demonstrate the use of python embedding'''
+
+def multiply():
+    print("test point 1 in py")
+    c = 12345*6789
+    print ('The result of 12345 x 6789 :', c)
+    return c
+
+
+def get_data():
+    import requests
+    weather_url="http://dataservice.accuweather.com/currentconditions/v1/206690?apikey=oFGptohDvJ6AtTgijcbjPLiqBXeWOol0&details=false"
+    weather_data=requests.get(weather_url).json()
+    print('weather data: ',weather_data)
+    return weather_data
